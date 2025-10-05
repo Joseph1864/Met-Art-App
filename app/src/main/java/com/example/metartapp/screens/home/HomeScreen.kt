fc63f4c737c56e522881e9166b2a7025cd6ef816
Joseph 1864 <josephwilliamsm99@gmail.com>
2025-10-04 21:44:29 -0700

    feat: added basic HomeScreen button and viewmodel

--- app/src/main/java/com/example/metartapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/metartapp/screens/home/HomeScreen.kt
@@ -1,11 +1,27 @@
 package com.example.metartapp.screens.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun HomeScreen() {
-    Text(
-        text = "Hello World!"
-    )
+fun HomeScreen(
+    viewModel: HomeScreenViewModel = HomeScreenViewModel(),
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            onClick = { viewModel.fetchArtwork() },
+        ) {
+            Text(text = "Click me to get an art object!")
+        }
+    }
 }
---
+++ app/src/main/java/com/example/metartapp/screens/home/HomeScreenViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.metartapp.screens.home
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.metartapp.data.artwork.Artwork
+import com.example.metartapp.data.artwork.ArtworkRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class HomeScreenViewModel(
+    private val artworkRepository: ArtworkRepository = ArtworkRepository(),
+): ViewModel() {
+
+    private val _viewState = MutableStateFlow(HomeScreenViewState())
+    val viewState = _viewState.asStateFlow()
+
+    fun fetchArtwork() {
+        viewModelScope.launch {
+            val artwork = artworkRepository.getRandomArtwork()
+            artwork?.let{
+                _viewState.update { currentState ->
+                    currentState.copy(artwork = it)
+                }
+            }
+        }
+    }
+
+}
+
+data class HomeScreenViewState(
+    val artwork: Artwork? = null,
+)
