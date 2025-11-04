c2b360bbefce93ecea015103879e927e3d8721b3
Joseph 1864 <josephwilliamsm99@gmail.com>
2025-11-03 21:17:19 -0800

    refactor: extracted the image composable into it's own file

--- app/src/main/java/com/example/metartapp/screens/artwork/ArtworkScreen.kt
+++ app/src/main/java/com/example/metartapp/screens/artwork/ArtworkScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Error
@@ -28,18 +27,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
-import coil.compose.SubcomposeAsyncImage
-import coil.compose.SubcomposeAsyncImageContent
 import com.example.metartapp.data.artwork.Artwork
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
@@ -105,39 +98,10 @@ fun Content(
     val imageUrl = artwork.primaryImageUrl ?: artwork.primaryImageSmallUrl
 
     if(!imageUrl.isNullOrEmpty()) {
-        SubcomposeAsyncImage(
-            model = imageUrl,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .fillMaxWidth()
-                .height(400.dp)
-                .clip(RoundedCornerShape(32.dp))
-        ) {
-            when (painter.state) {
-                is AsyncImagePainter.State.Loading -> {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        CircularProgressIndicator()
-                    }
-                }
-
-                is AsyncImagePainter.State.Error -> {
-                    ImagePlaceholder(
-                        modifier = Modifier
-                            .fillMaxSize()
-                    )
-                }
-
-                else -> {
-                    SubcomposeAsyncImageContent()
-                }
-            }
-        }
+        Image(
+            imageUrl = imageUrl,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
     } else {
         ImagePlaceholder(
             modifier = Modifier
