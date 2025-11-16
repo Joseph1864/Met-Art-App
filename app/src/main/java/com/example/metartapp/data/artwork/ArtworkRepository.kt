905451cdd48dfb85139d2be2b42edcfd628657ff
Joseph 1864 <josephwilliamsm99@gmail.com>
2025-11-15 18:47:12 -0800

    feat: added a basic artwork cache

--- app/src/main/java/com/example/metartapp/data/artwork/ArtworkRepository.kt
+++ app/src/main/java/com/example/metartapp/data/artwork/ArtworkRepository.kt
@@ -5,9 +5,15 @@ import com.example.metartapp.data.remote.MetApi
 class ArtworkRepository(
     private val api: MetApi,
 ) {
+    private val cache = mutableMapOf<Int, Artwork>()
+
     suspend fun getArtworkById(artworkId: Int): Artwork? {
         return try {
+            cache[artworkId]?.let { return it }
+
             val dto = api.getArtworkById(artworkId)
+            cache[artworkId] = dto.toDomainModel()
+
             dto.toDomainModel()
         } catch (e: Exception) {
             null
@@ -16,9 +22,13 @@ class ArtworkRepository(
 
     suspend fun getArtworkIdsWithImages(): IntArray? {
         return try {
-            api.getArtworkIdsWithImages().objectIds?.toIntArray()
+            api.getArtworkIdsWithImages().objectIDs?.toIntArray()
         } catch (e: Exception) {
             null
         }
+    }
+
+    fun saveArtworkToCache(artwork: Artwork) {
+        cache[artwork.id] = artwork
     }
 }
